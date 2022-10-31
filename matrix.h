006a97237195b03c9a4c35db401c8161c6bfc253
BVChamarthi <bharathvarma169@gmail.com>
2022-10-31 10:53:52 -0400

    defined constructors and destructor

--- matrix.h
+++ matrix.h
@@ -9,8 +9,10 @@ template<class T>
 class matrix2 {         //template class for 2d matrix
 
     private:
-        int row, col;
+        int row, col, num_elems;
         T* data;
+
+        int index(int r, int c);  // function to convert 2d coords to index in 1d array
 
     public:
         // constructors
@@ -30,30 +32,63 @@ class matrix2 {         //template class for 2d matrix
         int rows();
         int cols();
 
+        // overload == operator
+        bool operator== (const matrix2<T>& rhs);
+
+        // overload addition operator
+        template <class U> friend matrix2<U> operator+ (const matrix2<U>& lhs, const matrix2<U>& rhs);
+        template <class U> friend matrix2<U> operator+ (const U& lhs, const matrix2<U>& rhs);
+        template <class U> friend matrix2<U> operator+ (const matrix2<U>& lhs, const U& rhs);
+
+        // overload subtraction operator
+        template <class U> friend matrix2<U> operator- (const matrix2<U>& lhs, const matrix2<U>& rhs);
+        template <class U> friend matrix2<U> operator- (const U& lhs, const matrix2<U>& rhs);
+        template <class U> friend matrix2<U> operator- (const matrix2<U>& lhs, const U& rhs);
+
+        // overload multiplication operator
+        template <class U> friend matrix2<U> operator* (const matrix2<U>& lhs, const matrix2<U>& rhs);
+        template <class U> friend matrix2<U> operator* (const U& lhs, const matrix2<U>& rhs);
+        template <class U> friend matrix2<U> operator* (const matrix2<U>& lhs, const U& rhs);
+
 };
-
-// overload == operator
-template <class T> bool operator== (const matrix2<T>&lhs, const matrix2<T>& rhs);
-
-// overload addition operator
-template <class T> matrix2<T> operator+ (const matrix2<T>& lhs, const matrix2<T>& rhs);
-template <class T> matrix2<T> operator+ (const T& lhs, const matrix2<T>& rhs);
-template <class T> matrix2<T> operator+ (const matrix2<T>& lhs, const T& rhs);
-
-// overload subtraction operator
-template <class T> matrix2<T> operator- (const matrix2<T>& lhs, const matrix2<T>& rhs);
-template <class T> matrix2<T> operator- (const T& lhs, const matrix2<T>& rhs);
-template <class T> matrix2<T> operator- (const matrix2<T>& lhs, const T& rhs);
-
-// overload multiplication operator
-template <class T> matrix2<T> operator* (const matrix2<T>& lhs, const matrix2<T>& rhs);
-template <class T> matrix2<T> operator* (const T& lhs, const matrix2<T>& rhs);
-template <class T> matrix2<T> operator* (const matrix2<T>& lhs, const T& rhs);
 
 
 /*******************************************************************************************************
 CONSTRUCTORS / DESTRUCTORS
 *******************************************************************************************************/
 // default constructor
+template <class T> matrix2<T>::matrix2(): row(1), col(1), num_elems(1) {
+    data = new T[1];
+    data[0] = 0.0;
+}
+
+// parametrized constructor, no data
+template <class T> matrix2<T>::matrix2(int r, int c): row(r), col(c), num_elems(r*c) {
+    data = new T[num_elems];
+    for(int i = 0; i < num_elems; i++) {
+        data[i] = 0.0;
+    }
+}
+
+// parametrized constructor with data
+template <class T> matrix2<T>::matrix2(int r, int c, const T* d): row(r), col(c), num_elems(r*c) {
+    data = new T[num_elems];
+    for(int i = 0; i < num_elems; i++) {
+        data[i] = d[i];
+    }
+}
+
+// copy constructor
+template <class T> matrix2<T>::matrix2(const matrix2<T>& obj): row(obj.row), col(obj.col), num_elems(obj.num_elems) {
+    data = new T[num_elems];
+    for(int i = 0; i < num_elems; i++) {
+        data[i] = obj.data[i];
+    }
+}
+
+// destructor
+template <class T> matrix2<T>::~matrix2() {
+    if(data != nullptr) delete data;
+}
 
 #endif // !MATRIX_H
