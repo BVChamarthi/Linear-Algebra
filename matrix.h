564c24a1d1205b0f6607a6b84a3845c458fd9c4e
BVChamarthi <bharathvarma169@gmail.com>
2022-10-31 15:33:15 -0400

    implemented addition, subtraction and multiplication

--- matrix.h
+++ matrix.h
@@ -129,7 +129,7 @@ OPERATOR OVERLOADS
 
 // the == operator
 template <class T> bool matrix2<T>::operator== (const matrix2<T>& rhs) {
-    if( (row != rhs.row) || (col != rhs.col) ) return false;
+    if( (row != rhs.rows) || (col != rhs.cols) ) return false;
 
     for(int i = 0; i < num_elems; i++) {
         if(data[i] != rhs.data[i]) return false;
@@ -137,6 +137,93 @@ template <class T> bool matrix2<T>::operator== (const matrix2<T>& rhs) {
     return true;
 }
 
+// matrix + matrix
+template <class U> matrix2<U> operator+ (const matrix2<U>& lhs, const matrix2<U>& rhs) {
+    if(lhs.rows != rhs.rows || lhs.cols != rhs.cols) return nullptr;
+    matrix2 ans(lhs.rows, lhs.cols);
+    for(int i = 0; i < ans.num_elems; i++) {
+        ans.data[i] = lhs.data[i] + rhs.data[i];
+    }
+    return ans;
+}
+
+// scalar + matrix
+template <class U> matrix2<U> operator+ (const U& lhs, const matrix2<U>& rhs) {
+    matrix2 ans(rhs.rows, rhs.cols);
+    for(int i = 0; i < ans.num_elems; i++) {
+        ans.data[i] = lhs + rhs.data[i];
+    }
+    return ans;
+}
+
+// matrix + scalar
+template <class U> matrix2<U> operator+ (const matrix2<U>& lhs, const U& rhs) {
+    matrix2 ans(lhs.rows, lhs.cols);
+    for(int i = 0; i < ans.num_elems; i++) {
+        ans.data[i] = lhs.data[i] + rhs;
+    }
+    return ans;
+}
+
+// matrix - matrix
+template <class U> matrix2<U> operator- (const matrix2<U>& lhs, const matrix2<U>& rhs) {
+    if(lhs.rows != rhs.rows || lhs.cols != rhs.cols) return nullptr;
+    matrix2 ans(lhs.rows, lhs.cols);
+    for(int i = 0; i < ans.num_elems; i++) {
+        ans.data[i] = lhs.data[i] - rhs.data[i];
+    }
+    return ans;
+}
+
+// scalar - matrix
+template <class U> matrix2<U> operator- (const U& lhs, const matrix2<U>& rhs) {
+    matrix2 ans(lrhs.rows, rhs.cols);
+    for(int i = 0; i < ans.num_elems; i++) {
+        ans.data[i] = lhs - rhs.data[i];
+    }
+    return ans;
+}
+
+// matrix - scalar
+template <class U> matrix2<U> operator- (const matrix2<U>& lhs, const U& rhs) {
+    matrix2 ans(lhs.rows, lhs.cols);
+    for(int i = 0; i < ans.num_elems; i++) {
+        ans.data[i] = lhs.data[i] - rhs;
+    }
+    return ans;
+}
+
+// matrix * matrix
+template <class U> matrix2<U> operator* (const matrix2<U>& lhs, const matrix2<U>& rhs) {
+    if(lhs.cols != rhs.rows) return nullptr;
+    matrix2 ans(lhs.rows, rhs.cols);
+    for(int i = 0; i < ans.rows; i++) {
+        for(int j = 0; j < ans.cols; j++) {
+            for(int k = 0; k < lhs.cols; k++) {
+                ans(i,j) += lhs(i, k) * rhs(k, j);
+            }
+        }
+    }
+    return ans;
+}
+
+// scalar * matrix
+template <class U> matrix2<U> operator* (const U& lhs, const matrix2<U>& rhs) {
+    matrix2 ans(rhs.rows, rhs.cols);
+    for(int i = 0; i < ans.num_elems; i++) {
+        ans.data[i] = lhs * rhs.data[i];
+    }
+    return ans;
+}
+
+// matrix * scalar
+template <class U> matrix2<U> operator* (const matrix2<U>& lhs, const U& rhs) {
+    matrix2 ans(lhs.rows, lhs.cols);
+    for(int i = 0; i < ans.num_elems; i++) {
+        ans.data[i] = lhs.data[i] * rhs;
+    }
+    return ans;
+}
 
 
 #endif // !MATRIX_H
